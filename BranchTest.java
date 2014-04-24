9138fc90497e98664fd3f45fbed0e42d42595d7f
astaeheli <astaeheli@eagles.ewu.edu>
2014-04-23 22:13:48 -0700

    Merge branch 'master' of https://github.com/astaeheli/cscd350-github-lab
    Conflicts:
    	BranchTest.java

--- BranchTest.java
+++ BranchTest.java
@@ -4,10 +4,6 @@ public class BranchTest
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-		System.out.println("Hello Branch Test, change 3");
-=======
-		System.out.println("Hello Branch Test, change 2");
->>>>>>> 0d07b44280c790977244e16a9a31e71f3d611fe5
+		System.out.println("Stuff and things");
 	}
 }
