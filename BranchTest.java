7b28bef689decb0503178edd03e8a9968b4d2f25
astaeheli <astaeheli@eagles.ewu.edu>
2014-04-23 21:36:42 -0700

    Revert "changed test file"
    This reverts commit d5670ede2dd1fe51f50546003f0c608cc08fbea4.

--- BranchTest.java
+++ BranchTest.java
@@ -4,6 +4,6 @@ public class BranchTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, this file has been changed!");
+		System.out.println("Hello Branch Test");
 	}
 }
