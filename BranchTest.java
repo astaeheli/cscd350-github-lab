b098eb4f16901a23f7e5177bd3afd5a2f4c2e211
astaeheli <astaeheli@eagles.ewu.edu>
2014-04-23 22:00:10 -0700

    f
    f

---
+++ BranchTest.java
@@ -0,0 +1,13 @@
+//this is to test branching/merging
+
+public class BranchTest
+{
+	public static void main(String[] args)
+	{
+<<<<<<< HEAD
+		System.out.println("Hello Branch Test, change 3");
+=======
+		System.out.println("Hello Branch Test, change 2");
+>>>>>>> 0d07b44280c790977244e16a9a31e71f3d611fe5
+	}
+}
